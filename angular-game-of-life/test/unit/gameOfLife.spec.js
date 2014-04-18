32f9109c83ef74a1d1ad531e8b5d734f385f5cd7
Colin PUY <colin.puy@bonitasoft.com>
2014-04-18 16:01:54 +0200

    [GameOfLife] first test on cells

--- angular-game-of-life/test/unit/gameOfLife.spec.js
+++ angular-game-of-life/test/unit/gameOfLife.spec.js
@@ -1,10 +1,22 @@
-
-var Grid = function(nbColumn, nbRows) {
+var Grid = function (nbColumn, nbRows) {
     this.nbColumn = nbColumn;
     this.nbRows = nbRows;
+
+    this.cells = [];
+    for (var i = 0; i < nbColumn; i++) {
+        var row = [];
+        for (var j = 0; j < nbRows; j++) {
+            row[j] = {alive: false};
+        }
+        this.cells[i] = row;
+    }
 }
 
-describe('grid', function()  {
+Grid.prototype.nextGeneration = function () {
+    return new Grid(this.nbColumn, this.nbRows);
+};
+
+describe('a grid', function()  {
 
     it('should be a two dimensional grid', function() {
         var grid = new Grid(4, 8);
@@ -14,6 +26,22 @@ describe('grid', function()  {
     });
 
     it('should be created with dead cells', function() {
-        // TODO
+        var grid = new Grid(2, 2);
+
+        expect(grid.cells).toEqual([[{alive: false}, {alive: false}], [{alive: false}, {alive: false}]]);
+    });
+});
+
+
+
+describe('a cell', function() {
+
+    it('should die if it has fewer than two live neighbours, as if caused by underpopulation', function() {
+        var grid = new Grid(2, 2);
+        grid.cells[1][1].alive = true;
+
+        grid = grid.nextGeneration();
+
+        expect(grid.cells[1][1].alive).toBeFalsy;
     });
 });
